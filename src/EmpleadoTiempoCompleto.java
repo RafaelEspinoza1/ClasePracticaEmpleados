6b58c196c19c06d5e1aa56faa8ac20065ee3cae4
RafaelEspinoza1 <rafael.espinoza484@std.uni.edu.ni>
2024-06-01 20:54:11 -0600

    Se agrega el proyecto

---
+++ src/EmpleadoTiempoCompleto.java
@@ -0,0 +1,14 @@
+public class EmpleadoTiempoCompleto extends Empleado {
+
+    private double salarioAnual;
+
+    public EmpleadoTiempoCompleto(String nombre, String numeroIdentificacion, double salarioAnual) {
+        super(nombre, numeroIdentificacion);
+        this.salarioAnual = salarioAnual;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return salarioAnual / 12; // Salario mensual
+    }
+}
